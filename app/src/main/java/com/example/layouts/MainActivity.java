8d7a2e54850133fb3552f872e3897733efdb26dc
Henrique Silva <henriquessilva463@gmail.com>
2023-09-24 13:19:18 -0300

    'm'

--- app/src/main/java/com/example/layouts/MainActivity.java
+++ app/src/main/java/com/example/layouts/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.layouts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    Intent it;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void chamaTelaPoupanca(View v){
+        it = new Intent(getApplicationContext(), Poupanca.class);
+        startActivity(it);
+    }
+    public void chamaTelaQuiz(View v){
+        it = new Intent(getApplicationContext(), Quiz.class);
+        startActivity(it);
+    }
+    public void chamaTelaRestaurante(View v){
+        it = new Intent(getApplicationContext(), Restaurante.class);
+        startActivity(it);
+    }
+    public void chamaTelaPizzaria(View v){
+        it = new Intent(getApplicationContext(), Pizzaria.class);
+        startActivity(it);
     }
 }
