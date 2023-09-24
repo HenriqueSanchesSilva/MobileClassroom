986cf858dd0b48555b79702353edc927ec19bfa1
Henrique Silva <henriquessilva463@gmail.com>
2023-09-24 12:18:59 -0300

    'layoutIntroduce

--- TelasExercicio/app/src/main/java/com/example/telasexercicio/ContaRestaurante.java
+++ TelasExercicio/app/src/main/java/com/example/telasexercicio/ContaRestaurante.java
@@ -3,6 +3,7 @@ package com.example.telasexercicio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 public class ContaRestaurante extends AppCompatActivity {
@@ -20,6 +21,20 @@ public class ContaRestaurante extends AppCompatActivity {
         edtTaxa = findViewById(R.id.edtTaxa);
         edtContaTotal = findViewById(R.id.edtContaTotal);
         edtValorPorPessoa = findViewById(R.id.edtValorPorPessoa);
+    }
 
+    public void calculaConta(View v){
+            double consumo = Double.parseDouble(edtConsumo.getText().toString());
+            double couvert = Double.parseDouble(edtCouvert.getText().toString());
+            double valor =  consumo + couvert;
+            double taxa = (valor/100)*10;
+            double pessoas = Double.parseDouble(edtContaPorPessoa.getText().toString());
+            String text = String.valueOf(taxa);
+            edtTaxa.setText(text);
+            text = String.valueOf(valor+taxa);
+            edtContaTotal.setText(text);
+            double vlrPerPessoa = (valor+taxa)/pessoas;
+            text = String.valueOf(vlrPerPessoa);
+            edtValorPorPessoa.setText(text);
     }
 }
